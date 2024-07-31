24c6793bb9d76995b7aca3e37eb2a6ae7f99f7ba
Orkun Ergün <104510596+orkunergun@users.noreply.github.com>
2024-07-31 08:24:18 +0000

    Revert "update gms path"
    This reverts commit 9641761599955f4b486763b728a32a99c92e7fe8.

--- config/common.mk
+++ config/common.mk
@@ -231,7 +231,7 @@ $(call inherit-product, vendor/aosp/config/bootanimation.mk)
 $(call inherit-product, vendor/aosp/config/fonts.mk)
 
 # GMS
-$(call inherit-product, vendor/gms/setup.mk)
+$(call inherit-product, vendor/gms/products/gms.mk)
 
 # OTA
 $(call inherit-product, vendor/aosp/config/ota.mk)
